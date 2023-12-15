5625dc9679a78fbff909e0fa9701d9313c65c637
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2023-12-15 16:14:34 +0900

    Added better error handling

--- terminator.go
+++ terminator.go
@@ -1,24 +1,53 @@
 package ex
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"runtime"
+)
+
+type deferred struct {
+	callback func() error
+	fromFile string
+	fromLine int
+}
 
 type Terminator struct {
-	callbacks []func() error
+	defers []deferred
 }
 
 // The terminator is aimed to be included without a pointer,
 // however we need one only when deferring stuff
 func (terminator *Terminator) Defer(callback func() error) {
-	terminator.callbacks = append(terminator.callbacks, callback)
+	// We do not handle the ok parameter here because we don't want to
+	// fail this operation, and there is nothing much we can do if the
+	// runtime information cannot be retrieved, so it can stay empty.
+	_, file, line, _ := runtime.Caller(1)
+
+	terminator.defers = append(terminator.defers, deferred{
+		callback: callback,
+		fromFile: file,
+		fromLine: line,
+	})
 }
 
 func (terminator *Terminator) Close() error {
 	var errs []error
 
 	// The callbacks must be destroyed from the most recent to the oldest
-	for callbackIndex := len(terminator.callbacks) - 1; callbackIndex >= 0; callbackIndex-- {
-		if err := terminator.callbacks[callbackIndex](); err != nil {
-			errs = append(errs, err)
+	for callbackIndex := len(terminator.defers) - 1; callbackIndex >= 0; callbackIndex-- {
+		deferred := terminator.defers[callbackIndex]
+		if err := deferred.callback(); err != nil {
+			var message = "error in deferred close"
+			if deferred.fromFile != "" {
+				message = fmt.Sprintf(
+					"error in deferred close initiated by %s:%d",
+					deferred.fromFile,
+					deferred.fromLine,
+				)
+			}
+
+			errs = append(errs, fmt.Errorf("%s, caused by %w", message, err))
 		}
 	}
 
--- terminator_test.go
+++ terminator_test.go
@@ -1,6 +1,8 @@
 package ex_test
 
 import (
+	"errors"
+	"regexp"
 	"testing"
 
 	"github.com/Seb-C/ex"
@@ -65,7 +67,6 @@ func TestTerminator(t *testing.T) {
 
 			objA := typeA{}
 			objB := typeB{a: objA}
-
 			objB.Defer(objA.Close)
 
 			counterA := 0
@@ -117,6 +118,74 @@ func TestTerminator(t *testing.T) {
 			assert.Nil(t, err)
 			assert.Equal(t, 1, counterA)
 			assert.Equal(t, 1, counterB)
+		})
+	})
+
+	t.Run("errors", func(t *testing.T) {
+		t.Run("all returned with debug info", func(t *testing.T) {
+			errA := errors.New("error A")
+			errB := errors.New("error B")
+
+			var terminator = ex.Terminator{}
+			terminator.Defer(func() error {
+				return errA
+			})
+			terminator.Defer(func() error {
+				return errB
+			})
+
+			resultErr := terminator.Close()
+
+			assert.NotNil(t, resultErr)
+			assert.ErrorIs(t, resultErr, errA)
+			assert.ErrorIs(t, resultErr, errB)
+			assert.Contains(t, resultErr.Error(), "terminator_test.go")
+		})
+		t.Run("deep error text", func(t *testing.T) {
+			type typeA struct {
+				ex.Terminator
+			}
+			type typeB struct {
+				ex.Terminator
+				a typeA
+			}
+			type typeC struct {
+				ex.Terminator
+				b1 typeB
+				b2 typeB
+			}
+
+			objA1 := typeA{}
+			objA1.Defer(func() error {
+				return errors.New("error X1")
+			})
+			objA1.Defer(func() error {
+				return errors.New("error Y1")
+			})
+
+			objA2 := typeA{}
+			objA2.Defer(func() error {
+				return errors.New("error X2")
+			})
+			objA2.Defer(func() error {
+				return errors.New("error Y2")
+			})
+
+			objB1 := typeB{a: objA1}
+			objB1.Defer(objA1.Close)
+
+			objB2 := typeB{a: objA2}
+			objB2.Defer(objA2.Close)
+
+			objC := typeC{b1: objB1, b2: objB2}
+			objC.Defer(objB1.Close)
+			objC.Defer(objB2.Close)
+
+			err := objC.Close()
+
+			assert.NotNil(t, err)
+			assert.Regexp(t, regexp.MustCompile("error in deferred close initiated by .*terminator_test.go:\\d+, caused by error in deferred close initiated by .*terminator_test.go:\\d+, caused by error in deferred close initiated by .*terminator_test.go:\\d+, caused by error Y2"), err.Error())
+			assert.Regexp(t, regexp.MustCompile("error in deferred close initiated by /.*terminator_test.go:\\d+, caused by error in deferred close initiated by .*/terminator_test.go:\\d+, caused by error in deferred close initiated by .*/terminator_test.go:\\d+, caused by error Y1"), err.Error())
 		})
 	})
 }
